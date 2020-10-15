1085404b8eae94c87690926562625c4bf25f7d42
fsbmat <fsbmat@gmail.com>
2020-10-15 14:05:00 -0300

    apagar

--- teste.R
+++ teste.R
@@ -1,1 +1,13 @@
-x=21
+library(tabulizer)
+library(dplyr)
+library(stringi)
+url <- 'http://www2.alerj.rj.gov.br/leideacesso/spic/arquivo/folha-de-pagamento-2018-01.pdf'
+d <- extract_tables(url, encoding = "UTF-8", pages = 1)
+
+if (!require("remotes")) {
+  install.packages("remotes")
+}
+# on 64-bit Windows
+remotes::install_github(c("ropensci/tabulizer"), INSTALL_opts = "--no-multiarch")
+# elsewhere
+remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"))
